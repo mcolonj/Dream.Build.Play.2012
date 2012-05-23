5686406737a3aefa26d06c1674598f4e43f69597
Michael Colon <michaelcolon@gohighbeta.com>
2012-05-23 15:22:33 -0400

    addressed Player.cs encapsulation issue.

--- Dream.Build.Play.2012/Dream.Build.Play.2012/Game1_alt.cs
+++ Dream.Build.Play.2012/Dream.Build.Play.2012/Game1_alt.cs
@@ -65,25 +65,20 @@ namespace Dream.Build.Play._2012
         KeyboardState currentKeyboardState;
         KeyboardState previousKeyboardState;
 
-        float playerMoveSpeed;
-
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
-        // Keep animations in dictionary
-        Dictionary<string, Animation> playerAnimation = new Dictionary<string, Animation>();
-        Animation currentAnimation;
 
         // sprite sheet reader/helper/config class
         SpriteSheet sheet;
 
         //player class
-        Player player1;
+        Player tuk;
 
         public Game1_alt()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            tuk = new Player();
         }
 
         /// <summary>
@@ -94,10 +89,7 @@ namespace Dream.Build.Play._2012
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            playerMoveSpeed = 1.75f;
-            player1 = new Player();
-
+            spriteBatch = new SpriteBatch(GraphicsDevice);
             base.Initialize();
         }
 
@@ -107,22 +99,16 @@ namespace Dream.Build.Play._2012
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            //sprite texture
+            // create new sprite sheet with texture
             Texture2D spriteSheet = Content.Load<Texture2D>("NakedSpriteMan");
-            //sprite sheet coordinates
             sheet = new SpriteSheet("Content/NakedSpriteMan.plist", spriteSheet);
-            //load bee animation from sheet coordinates
-            LoadAnimations();
-            currentAnimation = playerAnimation["down"];
-            //player position
+           
+            // Initialize Player Tuk
             Vector2 playerPosition = new Vector2(
                     GraphicsDevice.Viewport.TitleSafeArea.X + GraphicsDevice.Viewport.TitleSafeArea.Width / 2,                                 
                     GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-            // initialize player with bee animation
-            player1.Initialize(currentAnimation, playerPosition);
+           
+            tuk.Initialize(playerPosition, sheet);
 
         }
 
@@ -142,18 +128,20 @@ namespace Dream.Build.Play._2012
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
+            // Allows the game to exit, cause it sucks.
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            // TODO: Add your update logic here
+            // TODO: Add Unnecessary Logic Here
             previousGamePadState = currentGamePadState;
             previousKeyboardState = currentKeyboardState;
 
             currentKeyboardState = Keyboard.GetState();
             currentGamePadState = GamePad.GetState(PlayerIndex.One);
 
-            UpdatePlayer(gameTime);
+            // map input & update palyer
+            Joystick input = MapInput(currentKeyboardState, currentGamePadState);
+            tuk.Update(gameTime, input);
 
             base.Update(gameTime);
         }
@@ -167,76 +155,10 @@ namespace Dream.Build.Play._2012
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin();
-            player1.Draw(spriteBatch);
-            spriteBatch.End();
+            spriteBatch.Begin(); // Begin Batch
+            tuk.Draw(spriteBatch);
+            spriteBatch.End();  // End Batch
             base.Draw(gameTime);
-        }
-
-        private void UpdatePlayer(GameTime gameTime)
-        {
-            Joystick input = MapInput(currentKeyboardState, currentGamePadState);
-            
-
-            player1.Position.X += input.X * playerMoveSpeed;
-            player1.Position.Y -= input.Y * playerMoveSpeed;
-
-            if (input.Left && input.Up)
-            {
-                currentAnimation = (Animation)playerAnimation["leftup"];
-                player1.Position.X -= playerMoveSpeed;
-                player1.Position.Y -= playerMoveSpeed;
-            }
-            else if (input.Left && input.Down)
-            {
-                currentAnimation = (Animation)playerAnimation["leftdown"];
-                player1.Position.X -= playerMoveSpeed;
-                player1.Position.Y += playerMoveSpeed;
-            }
-            else if (input.Right && input.Down)
-            {
-                currentAnimation = (Animation)playerAnimation["rightdown"];
-                player1.Position.X += playerMoveSpeed;
-                player1.Position.Y += playerMoveSpeed;
-            }
-            else if (input.Right && input.Up)
-            {
-                currentAnimation = (Animation)playerAnimation["rightup"];
-                player1.Position.X += playerMoveSpeed;
-                player1.Position.Y -= playerMoveSpeed;
-            }
-            else if (input.Left)
-            {
-                currentAnimation = (Animation)playerAnimation["left"];
-                player1.Position.X -= playerMoveSpeed;
-            }
-            else if (input.Right)
-            {
-                currentAnimation = (Animation)playerAnimation["right"];
-                player1.Position.X += playerMoveSpeed;
-            }
-            else if (input.Up)
-            {
-                currentAnimation = (Animation)playerAnimation["up"];
-                player1.Position.Y -= playerMoveSpeed;
-            }
-            else if (input.Down)
-            {
-                currentAnimation = (Animation)playerAnimation["down"];
-                player1.Position.Y += playerMoveSpeed;
-            }
-            else
-            {
-
-            }
-
-            ClearAnim();
-            player1.Update(currentAnimation, gameTime);
-        }
-
-        private void ClearAnim()
-        {
-          
         }
 
         //
@@ -258,45 +180,5 @@ namespace Dream.Build.Play._2012
             return joystick;
         }
 
-        //
-        // Load Player Animations
-        //
-        void LoadAnimations()
-        {
-            int animationTime = 100;
-
-            playerAnimation.Add("down", sheet.AnimationForFrameNames(
-                new string[] { "walkdown0.png", "walkdown1.png", "walkdown2.png", "walkdown3.png", "walkdown4.png", "walkdown5.png", "walkdown6.png", "walkdown7.png" },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("up", sheet.AnimationForFrameNames(
-                new string[] { "walkup0.png", "walkup1.png", "walkup2.png", "walkup3.png", "walkup4.png", "walkup5.png", "walkup6.png", "walkup7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("left", sheet.AnimationForFrameNames(
-                new string[] { "walkleft0.png", "walkleft1.png", "walkleft2.png", "walkleft3.png", "walkleft4.png", "walkleft5.png", "walkleft6.png", "walkleft7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("right", sheet.AnimationForFrameNames(
-                new string[] { "walkright0.png", "walkright1.png", "walkright2.png", "walkright3.png", "walkright4.png", "walkright5.png", "walkright6.png", "walkright7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("rightup", sheet.AnimationForFrameNames(
-                new string[] { "walkrightup0.png", "walkrightup1.png", "walkrightup2.png", "walkrightup3.png", "walkrightup4.png", "walkrightup5.png", "walkrightup6.png", "walkrightup7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("rightdown", sheet.AnimationForFrameNames(
-                new string[] { "walkrightdown0.png", "walkrightdown1.png", "walkrightdown2.png", "walkrightdown3.png", "walkrightdown4.png", "walkrightdown5.png", "walkrightdown6.png", "walkrightdown7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("leftdown", sheet.AnimationForFrameNames(
-                new string[] { "walkleftdown0.png", "walkleftdown1.png", "walkleftdown2.png", "walkleftdown3.png", "walkleftdown4.png", "walkleftdown5.png", "walkleftdown6.png", "walkleftdown7.png", },
-                animationTime, Color.White, true));
-
-            playerAnimation.Add("leftup", sheet.AnimationForFrameNames(
-                new string[] { "walkleftup0.png", "walkleftup1.png", "walkleftup2.png", "walkleftup3.png", "walkleftup4.png", "walkleftup5.png", "walkleftup6.png", "walkleftup7.png", },
-                animationTime, Color.White, true));
-
-        }
     }
 }
--- Dream.Build.Play.2012/Dream.Build.Play.2012/Player.cs
+++ Dream.Build.Play.2012/Dream.Build.Play.2012/Player.cs
@@ -28,6 +28,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Dream.Build.Play._2012.Plists;
 
 
 namespace Dream.Build.Play._2012
@@ -36,44 +37,156 @@ namespace Dream.Build.Play._2012
     {
         const int MAXHEALTH = 100;
         const int MAXENERGY = 100;
-        //const int PLAYERSPEED = 100;
 
+        private float speed;
+        private Vector2 position;
 
-        Animation playerAnimation;
+        // Keep animations in dictionary
+        Dictionary<string, Animation> playerAnimation = new Dictionary<string, Animation>();
+        Animation currentAnimation;
+        //Animation playerAnimation;
         Texture2D displayFrame { set; get; }
 
-        public Vector2 Position;
-        public bool Active;
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Player()
+        {
+            speed = 2.0f;
+        }
 
         public void Initialize(Animation animation, Vector2 location)
         {
-            playerAnimation = animation;
-            Position = location;
-            Active = false;
+            currentAnimation = animation;
+            position = location;
         }
         public void Initialize(Vector2 location)
         {
-            Position = location;
-            Active = false;
+            position = location;
         }
 
-        public void Update(Animation animation, GameTime gameTime)
+        public void Initialize(Vector2 location, SpriteSheet sheet)
         {
-            if (playerAnimation != animation)
+            LoadAnimations(sheet);
+            currentAnimation = playerAnimation["down"];
+            position = location;
+        }
+
+        public void Update(GameTime gameTime, Joystick input)
+        {
+            position.X += input.X * speed;
+            position.Y -= input.Y * speed;
+
+            if (input.Left && input.Up)
             {
-                playerAnimation = animation;
-            
+                currentAnimation = (Animation)playerAnimation["leftup"];
+                position.X -= speed;
+                position.Y -= speed;
             }
-            playerAnimation.Position = Position;
-            playerAnimation.Update(gameTime);
+            else if (input.Left && input.Down)
+            {
+                currentAnimation = (Animation)playerAnimation["leftdown"];
+                position.X -= speed;
+                position.Y += speed;
+            }
+            else if (input.Right && input.Down)
+            {
+                currentAnimation = (Animation)playerAnimation["rightdown"];
+                position.X += speed;
+                position.Y += speed;
+            }
+            else if (input.Right && input.Up)
+            {
+                currentAnimation = (Animation)playerAnimation["rightup"];
+                position.X += speed;
+                position.Y -= speed;
+            }
+            else if (input.Left)
+            {
+                currentAnimation = (Animation)playerAnimation["left"];
+                position.X -= speed;
+            }
+            else if (input.Right)
+            {
+                currentAnimation = (Animation)playerAnimation["right"];
+                position.X += speed;
+            }
+            else if (input.Up)
+            {
+                currentAnimation = (Animation)playerAnimation["up"];
+                position.Y -= speed;
+            }
+            else if (input.Down)
+            {
+                currentAnimation = (Animation)playerAnimation["down"];
+                position.Y += speed;
+            }
+            else
+            {
+               
+            }
+           
+            currentAnimation.Position = position;
+            currentAnimation.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            playerAnimation.Draw(spriteBatch);
+            currentAnimation.Draw(spriteBatch);
         }
 
 
+        public void AddAnimation(String key, Animation a)
+        {
+            playerAnimation.Add(key, a);
+        }
+
+        public void LoadAnimations(SpriteSheet sheet)
+        {
+            int animationTime = 100;
+
+            AddAnimation("down", sheet.AnimationForFrameNames(
+                new string[] { "walkdown0.png", "walkdown1.png", "walkdown2.png", "walkdown3.png", "walkdown4.png", "walkdown5.png", "walkdown6.png", "walkdown7.png" },
+                animationTime, Color.White, true));
+
+            AddAnimation("up", sheet.AnimationForFrameNames(
+                new string[] { "walkup0.png", "walkup1.png", "walkup2.png", "walkup3.png", "walkup4.png", "walkup5.png", "walkup6.png", "walkup7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("left", sheet.AnimationForFrameNames(
+                new string[] { "walkleft0.png", "walkleft1.png", "walkleft2.png", "walkleft3.png", "walkleft4.png", "walkleft5.png", "walkleft6.png", "walkleft7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("right", sheet.AnimationForFrameNames(
+                new string[] { "walkright0.png", "walkright1.png", "walkright2.png", "walkright3.png", "walkright4.png", "walkright5.png", "walkright6.png", "walkright7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("rightup", sheet.AnimationForFrameNames(
+                new string[] { "walkrightup0.png", "walkrightup1.png", "walkrightup2.png", "walkrightup3.png", "walkrightup4.png", "walkrightup5.png", "walkrightup6.png", "walkrightup7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("rightdown", sheet.AnimationForFrameNames(
+                new string[] { "walkrightdown0.png", "walkrightdown1.png", "walkrightdown2.png", "walkrightdown3.png", "walkrightdown4.png", "walkrightdown5.png", "walkrightdown6.png", "walkrightdown7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("leftdown", sheet.AnimationForFrameNames(
+                new string[] { "walkleftdown0.png", "walkleftdown1.png", "walkleftdown2.png", "walkleftdown3.png", "walkleftdown4.png", "walkleftdown5.png", "walkleftdown6.png", "walkleftdown7.png", },
+                animationTime, Color.White, true));
+
+            AddAnimation("leftup", sheet.AnimationForFrameNames(
+                new string[] { "walkleftup0.png", "walkleftup1.png", "walkleftup2.png", "walkleftup3.png", "walkleftup4.png", "walkleftup5.png", "walkleftup6.png", "walkleftup7.png", },
+                animationTime, Color.White, true));
+
+        }
 
     }
 }
